fd500106191bcee65634b310418e8546ba87e96b
korany0410 <kkkmzxx@gmail.com>
2023-03-03 15:15:04 +0900

    babbling

---
+++ coding test/src/lv0/Gurgle_1.java
@@ -0,0 +1,29 @@
+package lv0;
+
+//옹알이 (1)
+//        문제 설명
+//        머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다.
+//        조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다.
+//        문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를
+//        return하도록 solution 함수를 완성해주세요.
+public class Gurgle_1 {
+    public static void main(String[] args) {
+            String[] babbling = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+            int answer = 0; //발음 할 수 있는 단어 개수
+
+            int aya = 0; int ye = 0; int woo = 0; int ma = 0;
+            for(int i=0; i<babbling.length; i++){
+
+
+
+            }//for
+
+
+
+
+
+
+    }//main
+
+
+}
